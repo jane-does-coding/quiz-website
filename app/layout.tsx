8d4941d60c4c9b0b8336dac1948a027b1ac6e5d5
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-07-12 10:19:31 -0600

    Banner text done yayyyyyyyyyyy

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap"
+					rel="stylesheet"
+				></link>
+			</head>
 			<body className={`dark ${inter.className}`}>{children}</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
-
 export default function Home() {
-	return <div>hello</div>;
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-neutral-50 p-0 m-0">
+			<h1
+				className="text-mask bg-clip-text text-transparent text-[40rem] sixcaps m-0 tracking-[-1rem] leading-[40rem] font-extrabold"
+				style={{
+					backgroundImage: "url(/banner.jpg)",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+					backgroundPosition: "center",
+				}}
+			>
+				STUDY BETTER
+			</h1>
+			<br />
+			<br />
+			<br />
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio repellat
+				exercitationem quae optio ipsam ad, temporibus ab excepturi. Est, harum?
+			</p>
+		</div>
+	);
 }
